6a2cf30639b2db261963b116f73f91e68e3a7a04
SohiniKar2 <sohinikar2@gmail.com>
2025-05-04 12:05:08 +0530

    index.js

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ const server = http.createServer((req, res) => {
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log(Server is running on port ${PORT});
+  console.log(`Server is running on port ${PORT}`);
 });
